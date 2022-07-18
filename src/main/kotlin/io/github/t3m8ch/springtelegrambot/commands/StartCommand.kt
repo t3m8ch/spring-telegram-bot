72462113b2655bfdf0f095258104f347bded14dd
Kudyakov Artem <t3m8ch@yandex.ru>
2022-07-18 19:43:38 +0400

    feat: Add StartCommandWithArgs command

--- src/main/kotlin/io/github/t3m8ch/springtelegrambot/commands/StartCommand.kt
+++ src/main/kotlin/io/github/t3m8ch/springtelegrambot/commands/StartCommand.kt
@@ -1,6 +1,8 @@
 package io.github.t3m8ch.springtelegrambot.commands
 
 import io.github.t3m8ch.springtelegrambot.telegramBotsFacade.interfaces.Command
+import io.github.t3m8ch.springtelegrambot.telegramBotsFacade.utils.getArgs
+import io.github.t3m8ch.springtelegrambot.telegramBotsFacade.utils.hasArgs
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Message
@@ -9,6 +11,15 @@ import org.telegram.telegrambots.meta.api.objects.Message
 class StartCommand : Command<Message>(Message::class, { it.text == "/start" }) {
     override fun execute(obj: Message) {
         val method = SendMessage.builder().chatId(obj.chatId).text("Start!").build()
+        client.execute(method)
+    }
+}
+
+@Component
+class StartCommandWithArgs : Command<Message>(Message::class, { it.text.startsWith("/start") && it.hasArgs() }) {
+    override fun execute(obj: Message) {
+        val args = obj.getArgs()
+        val method = SendMessage.builder().chatId(obj.chatId).text(args.toString()).build()
         client.execute(method)
     }
 }
